8aee04264054c321bb3fc1712502f991febcf780
raosa <raosa@DESKTOP-MVUNLVF>
2022-02-27 17:11:34 +0530

    25th february Assignment

---
+++ Assignment of 25th feb/src/halfDiamondStar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class halfDiamondStar {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number");
+		int n = sc.nextInt();
+		int s=0;
+		 for(int i= n-1; i>= 0; i--)
+	        {
+	            for(int j = n-1;j>s;j--)
+	           
+	                System.out.print(" ");
+	            System.out.print("*");
+	            for(int k=1;k<s;k++)
+	           
+	                System.out.print("");
+	            if(i<n-1)
+	            
+	                System.out.print("*");
+	            s++;
+	            System.out.println();
+	        }
+	}
+
+}
